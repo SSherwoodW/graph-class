b4dd9bf2ed799b8a2e32879fe64fe06c05e4c7e7
Scott Waller <ssherwoodw@gmail.com>
2023-11-01 15:31:25 -0700

    adding

---
+++ graph.js
@@ -0,0 +1,92 @@
+class Node {
+    constructor(value, adjacent = new Set()) {
+        this.value = value;
+        this.adjacent = adjacent;
+    }
+}
+
+class Graph {
+    constructor() {
+        this.nodes = new Set();
+    }
+
+    // this function accepts a Node instance and adds it to the nodes property on the graph
+    addVertex(vertex) {
+        this.nodes.add(vertex);
+    }
+
+    // this function accepts an array of Node instances and adds them to the nodes property on the graph
+    addVertices(vertexArray) {
+        for (let vertex of vertexArray) {
+            this.nodes.add(vertex);
+        }
+    }
+
+    // this function accepts two vertices and updates their adjacent values to include the other vertex
+    addEdge(v1, v2) {
+        v1.adjacent.add(v2);
+        v2.adjacent.add(v1);
+    }
+
+    // this function accepts two vertices and updates their adjacent values to remove the other vertex
+    removeEdge(v1, v2) {
+        if (v1.adjacent.has(v2) && v2.adjacent.has(v1)) {
+            v1.adjacent.delete(v2);
+            v2.adjacent.delete(v1);
+        }
+    }
+
+    // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+    removeVertex(vertex) {
+        for (let node of this.nodes) {
+            if (node.adjacent.has(vertex)) {
+                node.adjacent.delete(vertex);
+            }
+        }
+        this.nodes.delete(vertex);
+    }
+
+    // this function returns an array of Node values using DFS
+    depthFirstSearch(start) {
+        let toVisitStack = [start];
+        let seen = new Set(toVisitStack);
+        let vals = [];
+        let currNode;
+
+        while (toVisitStack.length > 0) {
+            currNode = toVisitStack.pop();
+            vals.push(currNode.value);
+
+            for (let neighbor of currNode.adjacent) {
+                if (!seen.has(neighbor)) {
+                    toVisitStack.push(neighbor);
+                    seen.add(neighbor);
+                }
+            }
+        }
+        return vals;
+    }
+
+    // this function returns an array of Node values using BFS
+    breadthFirstSearch(start) {
+        let toVisitQueue = [start];
+        let seen = new Set(toVisitQueue);
+        let vals = [];
+        let currNode;
+
+        while (toVisitQueue.length > 0) {
+            currNode = toVisitQueue.shift();
+            vals.push(currNode.value);
+
+            for (let neighbor of currNode.adjacent) {
+                if (!seen.has(neighbor)) {
+                    toVisitQueue.push(neighbor);
+                    seen.add(neighbor);
+                }
+            }
+        }
+        return vals;
+    }
+}
+
+module.exports = { Graph, Node };
